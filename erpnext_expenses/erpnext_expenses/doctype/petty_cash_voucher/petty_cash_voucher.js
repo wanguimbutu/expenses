149a30156e6bd16f6d56bef52e84a7ed1f77017c
wanguimbutu <wanguimbutu@gmail.com>
2025-06-12 16:36:40 +0300

    remove frontend calculation of petty cash voucher

--- erpnext_expenses/erpnext_expenses/doctype/petty_cash_voucher/petty_cash_voucher.js
+++ erpnext_expenses/erpnext_expenses/doctype/petty_cash_voucher/petty_cash_voucher.js
@@ -19,17 +19,14 @@ frappe.ui.form.on('Petty Cash Voucher', {
         });
     },
 
+    // Don't calculate totals in frontend anymore
     validate: function(frm) {
-        calculate_totals(frm);
+        // Do nothing - backend will handle all totals
     }
 });
 
 frappe.ui.form.on('Petty Cash Details', {
-    debit: function(frm) {
-        calculate_totals(frm);
-    },
     petty_cash_details_add: function(frm, cdt, cdn) {
-        calculate_totals(frm);
         const row = locals[cdt][cdn];
         if (frm.doc.company) {
             frappe.call({
@@ -47,9 +44,7 @@ frappe.ui.form.on('Petty Cash Details', {
             });
         }
     },
-    petty_cash_details_remove: function(frm) {
-        calculate_totals(frm);
-    },
+
     cost_center: function(frm, cdt, cdn) {
         const row = locals[cdt][cdn];
         if (!row.cost_center && frm.doc.company) {
@@ -70,65 +65,4 @@ frappe.ui.form.on('Petty Cash Details', {
     }
 });
 
-frappe.ui.form.on('Petty Cash Vat Details', {
-    amount: function(frm) {
-        calculate_totals(frm);
-    },
-    vat_account: function(frm) {
-        calculate_totals(frm);
-    },
-    vat_details_add: function(frm) {
-        calculate_totals(frm);
-    },
-    vat_details_remove: function(frm) {
-        calculate_totals(frm);
-    }
-});
-
-frappe.ui.form.on('Petty Cash Items', {
-    qty: function(frm, cdt, cdn) {
-        update_amount_and_totals(frm, cdt, cdn);
-    },
-    rate: function(frm, cdt, cdn) {
-        update_amount_and_totals(frm, cdt, cdn);
-    },
-    petty_cash_items_add: function(frm) {
-        calculate_totals(frm);
-    },
-    petty_cash_items_remove: function(frm) {
-        calculate_totals(frm);
-    }
-});
-
-function update_amount_and_totals(frm, cdt, cdn) {
-    const row = locals[cdt][cdn];
-    const amount = flt(row.qty) * flt(row.rate);
-
-    frappe.model.set_value(cdt, cdn, "amount", amount).then(() => {
-        calculate_totals(frm);
-    });
-}
-
-function calculate_totals(frm) {
-    let total = 0;
-    let total_vat = 0;
-    let items_total = 0;
-
-    frm.doc.petty_cash_details.forEach(row => {
-        total += flt(row.debit);
-    });
-
-    frm.doc.vat_details.forEach(row => {
-        total_vat += flt(row.amount);
-    });
-
-    frm.doc.petty_cash_items.forEach(row => {
-        items_total += flt(row.qty) * flt(row.rate);
-    });
-
-    frm.set_value("total", total);
-    frm.set_value("total_vat", total_vat);
-    frm.set_value("items_total", items_total);
-
-    frm.set_value("amount", flt(total) + flt(total_vat) + flt(items_total));
-}
+// No amount calculations for Petty Cash Items or VAT in frontend anymore
